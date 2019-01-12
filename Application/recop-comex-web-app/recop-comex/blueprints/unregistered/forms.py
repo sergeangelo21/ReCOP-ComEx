89109a09d921e6f64f64ed3316142c1bc82fe85e
ivan ashley <jaminal_ivan@yahoo.com.ph>
2019-01-12 19:16:25 +0800

    terms and services

---
+++ Application/recop-comex-web-app/recop-comex/blueprints/unregistered/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, TextAreaField, PasswordField, SubmitField, SelectField, DecimalField, DateField, BooleanField, RadioField
+from wtforms.validators import DataRequired, EqualTo, ValidationError, NumberRange, Email
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    middlename = StringField('Middle Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    gender = RadioField('Gender', choices=[("M","Male"),("F","Female")], validators=[DataRequired()])
+    company = StringField('Company Name', validators=[DataRequired()])
+    birthday = DateField('Birthday', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    telephone = StringField('Telephone Number', validators=[DataRequired()])
+    mobile = StringField('Mobile Number', validators=[DataRequired()])
+    type = RadioField('I am a', choices=[("1","Participant"),("2","Partner"),("3","Beneficiary")], validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
