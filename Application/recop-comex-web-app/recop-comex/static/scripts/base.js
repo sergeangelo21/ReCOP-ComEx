ac382239b7875f91b268cd3264c8c5ed400749c5
sergeangelo21 <sergeangelomajillo@gmail.com>
2019-01-21 08:27:10 +0800

    removed slow loading images in signup

--- Application/recop-comex-web-app/recop-comex/static/scripts/base.js
+++ Application/recop-comex-web-app/recop-comex/static/scripts/base.js
@@ -1,10 +1,11 @@
 window.onload = function() {
-var burger = document.querySelector('.burger');
-var nav = document.querySelector('#'+burger.dataset.target);
 
-burger.addEventListener('click', function(){
-  burger.classList.toggle('is-active');
-  nav.classList.toggle('is-active');
-})
+	var burger = document.querySelector('.burger');
+	var nav = document.querySelector('#'+burger.dataset.target);
+
+	burger.addEventListener('click', function(){
+  		burger.classList.toggle('is-active');
+ 		nav.classList.toggle('is-active');
+ 	})
 
 }
