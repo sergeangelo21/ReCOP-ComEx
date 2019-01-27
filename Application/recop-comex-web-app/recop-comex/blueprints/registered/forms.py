dc36a77a3f79b67ca0b0617d5fa14aa9fefa550e
AlexAustin <alexaustinquintin@gmail.com>
2019-01-27 18:37:07 +0800

    registered.profile | query and edit okay but edit password not yet

---
+++ Application/recop-comex-web-app/recop-comex/blueprints/registered/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, TextAreaField, PasswordField, SubmitField, SelectField, DecimalField, DateField, BooleanField, RadioField
+from wtforms.validators import DataRequired, EqualTo, ValidationError, NumberRange, Email
+
+class ProfileUpdateForm(FlaskForm):
+    firstname = StringField('First Name')
+    middlename = StringField('Middle Name')
+    lastname = StringField('Last Name')
+    gender = RadioField('Gender', choices=[("M","Male"),("F","Female")])
+    birthday = DateField('Birthday')
+    bio = StringField('Bio')
+    company = StringField('Company Name')    
+    address = StringField('Address')
+    telephone = StringField('Telephone')
+    mobile = StringField('Mobile Number')
+    email = StringField('Email Address')
+    username = StringField('Username')
+    password = PasswordField('Password')
+    submit = SubmitField('Update')
--- Application/recop-comex-web-app/recop-comex/blueprints/registered/views.py
+++ Application/recop-comex-web-app/recop-comex/blueprints/registered/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect, flash
 from flask_login import current_user, login_required
-from data_access.models import user_account, user_information
+from blueprints.registered.forms import *
+from data_access.models import *
 
+from extensions import db, bcrypt
 import os
 
 registered = Blueprint('registered', __name__, template_folder="templates")
@@ -36,8 +38,6 @@ def events():
 @login_required
 def events_create():
 
-
-
 	return render_template('/registered/events/create.html', )
 
 @registered.route('/registered/partners')
@@ -64,8 +64,94 @@ def termsandconditions():
 
 	return render_template('/registered/termsandconditions.html')
 
-@registered.route('/registered/profile')
+@registered.route('/registered/profile/<registered>')
 @login_required
-def profile():
+def profile(registered):
 
-	return render_template('/registered/profile.html')
+	registered = user_information.query.join(
+		user_account
+		).add_columns(
+		user_information.first_name,
+		user_information.middle_name,
+		user_information.last_name,
+		user_information.company_name,
+		user_information.bio,
+		user_information.gender,
+		user_information.birthday,
+		user_information.address,
+		user_information.address,
+		user_information.telephone,
+		user_information.mobile_number,
+		user_account.username,
+		user_account.password,
+		user_account.email_address
+		).filter_by(id=current_user.id).first()
+
+	return render_template('/registered/profile/profile.html', registered=registered)
+
+@registered.route('/registered/profile/update/<registered>', methods=['GET', 'POST'])
+@login_required
+def profile_update(registered):
+
+	registered = user_information.query.join(
+		user_account
+		).add_columns(
+		user_information.first_name,
+		user_information.middle_name,
+		user_information.last_name,
+		user_information.company_name,
+		user_information.bio,
+		user_information.gender,
+		user_information.birthday,
+		user_information.address,
+		user_information.address,
+		user_information.telephone,
+		user_information.mobile_number,
+		user_account.username,
+		user_account.password,
+		user_account.email_address
+		).filter_by(id=current_user.id).first()
+
+	form = ProfileUpdateForm()
+
+	if form.validate_on_submit():
+
+		registered.user_information.first_name = form.firstname.data
+		registered.user_information.middle_name = form.middlename.data
+		registered.user_information.last_name = form.lastname.data
+		registered.user_information.gender = form.gender.data
+		registered.user_information.birthday = form.birthday.data
+		registered.user_information.bio = form.bio.data
+
+		registered.user_information.company_name = form.company.data
+		registered.user_information.address = form.address.data
+		registered.user_information.telephone = form.telephone.data
+		registered.user_information.mobile_number = form.mobile.data
+		registered.user_information.email_address = form.email.data
+
+		registered.user_information.username = form.username.data
+		registered.user_information.password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+
+		db.session.commit()
+
+		return redirect(url_for('registered.profile', registered=current_user.id))
+
+	else:
+
+		form.firstname.data = registered.first_name
+		form.middlename.data = registered.middle_name
+		form.lastname.data = registered.last_name
+		form.gender.data = registered.gender
+		form.birthday.data = registered.birthday
+		form.bio.data = registered.bio
+		
+		form.company.data = registered.company_name
+		form.address.data = registered.address
+		form.telephone.data = registered.telephone
+		form.mobile.data = registered.mobile_number
+		form.email.data = registered.email_address
+
+		form.username.data = registered.username
+		form.password.data = registered.password
+
+	return render_template('/registered/profile/profile_update.html', form=form, registered=registered)
