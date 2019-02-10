32d9461e6cabdcf01f3391ec1977fada24865212
marianaustria <austriamar1098@gmail.com>
2019-02-10 21:10:47 +0800

    Unregistered | event, linkages (filter shrink)

--- Application/recop-comex-web-app/recop-comex/static/scripts/unregistered.js
+++ Application/recop-comex-web-app/recop-comex/static/scripts/unregistered.js
@@ -174,3 +174,13 @@ function btn(sender){
     }
 
 }
+
+
+function filtershrink() {
+    var div = document.getElementById("filterDIV");
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
