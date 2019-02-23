4dba16221d6bec179ec7c826a4bb469e920f1128
ivan ashley <jaminal_ivan@yahoo.com.ph>
2019-02-23 14:58:35 +0800

    forms

--- Application/recop-comex-web-app/recop-comex/blueprints/registered/forms.py
+++ Application/recop-comex-web-app/recop-comex/blueprints/registered/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField, TextAreaField, PasswordField, SubmitField, SelectField, DecimalField, DateField, BooleanField, RadioField, IntegerField
+from wtforms import StringField, TextAreaField, PasswordField, SubmitField, SelectField, DecimalField, DateField, BooleanField, RadioField, IntegerField, validators
 from wtforms.validators import DataRequired, EqualTo, ValidationError, NumberRange, Email
 
 class DonationForm(FlaskForm):
@@ -14,19 +14,19 @@ class DonationForm(FlaskForm):
     submit = SubmitField('Donate')
 
 class ProfilePersonalUpdateForm(FlaskForm):
-    firstname = StringField('First Name')
-    middlename = StringField('Middle Name')
-    lastname = StringField('Last Name')
+    firstname = StringField('First Name', [validators.Length(min=0, max=30)])
+    middlename = StringField('Middle Name', [validators.Length(min=0, max=20)])
+    lastname = StringField('Last Name', [validators.Length(min=0, max=20)])
     gender = RadioField('Gender', choices=[("M","Male"),("F","Female")])
     birthday = DateField('Birthday')
-    bio = StringField('Bio')
+    bio = StringField('Bio', [validators.Length(min=0, max=160)])
     submit = SubmitField('Update')
 
 class ProfileContactUpdateForm(FlaskForm):
-    address = StringField('Address')
-    telephone = IntegerField('Telephone Number')
-    mobile = IntegerField('Mobile Number')
-    email = StringField('Email Address')
+    address = StringField('Address', [validators.Length(min=10, max=100)])
+    telephone = StringField('Telephone Number', [validators.Length(min=7, max=15)])
+    mobile = StringField('Mobile Number', [validators.Length(min=11, max=25)])
+    email = StringField('Email Address', [validators.Length(min=0, max=30)] )
     submit = SubmitField('Update')
 
 class ProfileUsernameUpdateForm(FlaskForm):
@@ -36,5 +36,5 @@ class ProfileUsernameUpdateForm(FlaskForm):
 
 class PasswordUpdateForm(FlaskForm):
     oldpassword = PasswordField('Old Password')
-    password = PasswordField('Password')
+    password = PasswordField('Password', [validators.Length(min=0, max=60)])
     submit = SubmitField('Update')
